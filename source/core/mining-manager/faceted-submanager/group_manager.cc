5685539a5baf74cca00c5c1b9bb820262e64f9e3
Jun Jiang <jun.zju@gmail.com>
2011-05-13 15:31:56 +0800

    fix a bug in GroupManager::getGroupRepFromTable().
    given "group_label" property value A, for each doc result, it only
    checks whether its property value equals to A, while it should check
    whether it is a child value of A.

--- source/core/mining-manager/faceted-submanager/group_manager.cc
+++ source/core/mining-manager/faceted-submanager/group_manager.cc
@@ -12,6 +12,43 @@ using namespace sf1r::faceted;
 #define DEBUG_PRINT_GROUP 0
 #define DEBUG_GROUPREP_FROM_DM 0
 
+namespace
+{
+
+/**
+ * Check whether @p v1 is the parent node of @p v2.
+ * @param parentTable table containing parent node for each node, 0 for root node
+ * @param v1 node 1
+ * @param v2 node 2
+ * @return true for @p v1 is the parent node of @p v2, otherwise false is returned,
+ *         when @p v1 equals to @p v2, true is returned,
+ *         when either @p v1 or @p v2 is zero for invalid id, false is returned.
+ */
+bool isParentValue(
+    const std::vector<PropValueTable::pvid_t>& parentTable,
+    PropValueTable::pvid_t v1,
+    PropValueTable::pvid_t v2
+)
+{
+    if (v1 == 0 || v2 == 0)
+        return false;
+
+    if (v2 >= parentTable.size())
+        return (v2 == v1);
+
+    while (v2 != 0)
+    {
+        if (v2 == v1)
+            return true;
+
+        v2 = parentTable[v2];
+    }
+
+    return false;
+}
+
+}
+
 GroupManager::GroupManager(
     DocumentManager* documentManager,
     const std::string& dirPath
@@ -323,7 +360,8 @@ bool GroupManager::getGroupRepFromTable(
                         if (*nameIt != condPVTable->propName())
                         {
                             const std::vector<PropValueTable::pvid_t>& condIdTable = condPVTable->propIdTable();
-                            if (docId < condIdTable.size() && condIdTable[docId] != condIt->second)
+                            if (docId < condIdTable.size()
+                                && isParentValue(condPVTable->parentIdTable(), condIt->second, condIdTable[docId]) == false)
                             {
                                 isMeetCond = false;
                                 break;
