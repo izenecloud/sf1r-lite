fc824df41c28d18739a32df959b81fb55f438c24
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2011-07-12 20:54:48 +0800

    fix bug of property diversity reranker

--- source/core/mining-manager/faceted-submanager/property_diversity_reranker.cc
+++ source/core/mining-manager/faceted-submanager/property_diversity_reranker.cc
@@ -43,7 +43,7 @@ void PropertyDiversityReranker::rerank(
     rankScoreList.reserve(numDoc);
 
     DocIdMap docIdMap;
-
+    DocIdList missDocs;
     for (std::size_t i = 0; i < numDoc; ++i)
     {
         docid_t docId = docIdList[i];
@@ -57,18 +57,34 @@ void PropertyDiversityReranker::rerank(
         {
             docIdMap[pvId].push_back(std::make_pair(docId, rankScoreList[i]));
         }
+        else
+        {
+            missDocs.push_back(std::make_pair(docId, rankScoreList[i]));
+        }
     }
-    if(docIdMap.size() == 1) return; // single property
+    if(docIdMap.size() <= 1) return; // single property or empty
     do{
         for(DocIdMap::iterator mapIt = docIdMap.begin(); mapIt != docIdMap.end(); ++mapIt)
         {
-            if(mapIt->second.empty()) continue;
-            std::pair<unsigned int, float> element = mapIt->second.front();
-            mapIt->second.pop_front();
-            newDocIdList.push_back(element.first);
-            newScoreList.push_back(element.second);
+            if(mapIt->second.empty()) 
+            {
+                docIdMap.erase(mapIt++);
+            }
+            else
+            {
+                std::pair<unsigned int, float> element = mapIt->second.front();
+                mapIt->second.pop_front();
+                newDocIdList.push_back(element.first);
+                newScoreList.push_back(element.second);
+            }
         }
-    }while(newDocIdList.size() < numDoc);
+    }while(!docIdMap.empty());
+
+    for(DocIdList::iterator missIt = missDocs.begin(); missIt != missDocs.end(); ++missIt)
+    {
+        newDocIdList.push_back(missIt->first);
+        newScoreList.push_back(missIt->second);
+    }
 
     using std::swap;
     swap(docIdList, newDocIdList);
