6eee80d58f6ecbe875bc138eee9e9199d8124ed2
Jun Jiang <jun.zju@gmail.com>
2011-07-15 09:45:15 +0800

    for API RecommendController::visit_item(), add param "session_id", so that the items recommended for VAV type in do_recommend() would only be limited in session range, instead of user's whole visit history, it could reduce the cost in updating visit matrix.

--- source/core/recommend-manager/VisitManager.cpp
+++ source/core/recommend-manager/VisitManager.cpp
@@ -8,20 +8,24 @@ namespace sf1r
 {
 
 VisitManager::VisitManager(
-    const std::string& path,
+    const std::string& visitDBPath,
+    const std::string& sessionDBPath,
     CoVisitManager* coVisitManager
 )
-    : container_(path)
+    : visitDB_(visitDBPath)
+    , sessionDB_(sessionDBPath)
     , coVisitManager_(coVisitManager)
 {
-    container_.open();
+    visitDB_.open();
+    sessionDB_.open();
 }
 
 void VisitManager::flush()
 {
     try
     {
-        container_.flush();
+        visitDB_.flush();
+        sessionDB_.flush();
     }
     catch(izenelib::util::IZENELIBException& e)
     {
@@ -29,10 +33,60 @@ void VisitManager::flush()
     }
 }
 
-bool VisitManager::addVisitItem(userid_t userId, itemid_t itemId)
+bool VisitManager::addVisitItem(
+    const std::string& sessionId,
+    userid_t userId,
+    itemid_t itemId
+)
+{
+    if (updateVisitDB_(userId, itemId)
+        && updateSessionDB_(sessionId, userId, itemId))
+    {
+        return true;
+    }
+
+    return false;
+}
+
+bool VisitManager::getVisitItemSet(userid_t userId, ItemIdSet& itemIdSet)
+{
+    bool result = false;
+    try
+    {
+        itemIdSet.clear();
+        visitDB_.getValue(userId, itemIdSet);
+        result = true;
+    }
+    catch(izenelib::util::IZENELIBException& e)
+    {
+        LOG(ERROR) << "exception in SDB::getValue(): " << e.what();
+    }
+
+    return result;
+}
+
+unsigned int VisitManager::visitUserNum()
+{
+    return visitDB_.numItems();
+}
+
+VisitManager::SDBIterator VisitManager::begin()
+{
+    return SDBIterator(visitDB_);
+}
+
+VisitManager::SDBIterator VisitManager::end()
+{
+    return SDBIterator();
+}
+
+bool VisitManager::updateVisitDB_(
+    userid_t userId,
+    itemid_t itemId
+)
 {
     ItemIdSet itemIdSet;
-    container_.getValue(userId, itemIdSet);
+    visitDB_.getValue(userId, itemIdSet);
 
     std::pair<ItemIdSet::iterator, bool> res = itemIdSet.insert(itemId);
     // not visited yet
@@ -41,11 +95,55 @@ bool VisitManager::addVisitItem(userid_t userId, itemid_t itemId)
         bool result = false;
         try
         {
-            result = container_.update(userId, itemIdSet);
+            result = visitDB_.update(userId, itemIdSet);
         }
         catch(izenelib::util::IZENELIBException& e)
         {
-            LOG(ERROR) << "exception in SDB::update(): " << e.what();
+            LOG(ERROR) << "exception in visit DB SDB::update(): " << e.what();
+        }
+
+        return result;
+    }
+
+    // already visited
+    return true;
+}
+
+bool VisitManager::updateSessionDB_(
+    const std::string& sessionId,
+    userid_t userId,
+    itemid_t itemId
+)
+{
+    if (sessionId.empty())
+    {
+        LOG(ERROR) << "empty session id is not allowed to update session db, userId: " << userId
+                   << ", itemId: " << itemId;
+        return false;
+    }
+
+    VisitSession visitSession;
+    sessionDB_.getValue(userId, visitSession);
+
+    // reset session
+    if (visitSession.sessionId_ != sessionId)
+    {
+        visitSession.sessionId_ = sessionId;
+        visitSession.itemSet_.clear();
+    }
+
+    std::pair<ItemIdSet::iterator, bool> res = visitSession.itemSet_.insert(itemId);
+    // not visited in current session
+    if (res.second)
+    {
+        bool result = false;
+        try
+        {
+            result = sessionDB_.update(userId, visitSession);
+        }
+        catch(izenelib::util::IZENELIBException& e)
+        {
+            LOG(ERROR) << "exception in visit session SDB::update(): " << e.what();
         }
 
         if (result)
@@ -53,8 +151,8 @@ bool VisitManager::addVisitItem(userid_t userId, itemid_t itemId)
             std::list<itemid_t> oldItems;
             std::list<itemid_t> newItems;
 
-            for (ItemIdSet::const_iterator it = itemIdSet.begin();
-                it != itemIdSet.end(); ++it)
+            for (ItemIdSet::const_iterator it = visitSession.itemSet_.begin();
+                it != visitSession.itemSet_.end(); ++it)
             {
                 if (*it != itemId)
                 {
@@ -72,40 +170,8 @@ bool VisitManager::addVisitItem(userid_t userId, itemid_t itemId)
         return result;
     }
 
-    // already visited
+    // already visited in current session
     return true;
-}
-
-bool VisitManager::getVisitItemSet(userid_t userId, ItemIdSet& itemIdSet)
-{
-    bool result = false;
-    try
-    {
-        itemIdSet.clear();
-        container_.getValue(userId, itemIdSet);
-        result = true;
-    }
-    catch(izenelib::util::IZENELIBException& e)
-    {
-        LOG(ERROR) << "exception in SDB::getValue(): " << e.what();
-    }
-
-    return result;
-}
-
-unsigned int VisitManager::visitUserNum()
-{
-    return container_.numItems();
-}
-
-VisitManager::SDBIterator VisitManager::begin()
-{
-    return SDBIterator(container_);
-}
-
-VisitManager::SDBIterator VisitManager::end()
-{
-    return SDBIterator();
 }
 
 } // namespace sf1r
--- test/recommend-manager/t_VisitManager.cpp
+++ test/recommend-manager/t_VisitManager.cpp
@@ -16,6 +16,8 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <sstream>
+#include <algorithm> // sort
 
 using namespace std;
 using namespace boost;
@@ -28,6 +30,7 @@ namespace
 const izenelib::util::UString::EncodingType ENCODING_TYPE = izenelib::util::UString::UTF_8;
 const char* TEST_DIR_STR = "recommend_test/t_VisitManager";
 const char* VISIT_DB_STR = "visit.db";
+const char* SESSION_DB_STR = "session.db";
 const char* COVISIT_DIR_STR = "covisit";
 }
 
@@ -69,6 +72,30 @@ void iterateVisitManager(const VisitMap& visitMap, VisitManager& visitManager)
     BOOST_CHECK_EQUAL(iterNum, visitManager.visitUserNum());
 }
 
+void checkCoVisitResult(
+    CoVisitManager* coVisitManager,
+    itemid_t inputItem,
+    const char* recItemStr
+)
+{
+    vector<itemid_t> goldRecItems;
+    istringstream ss(recItemStr);
+    itemid_t itemId;
+    while (ss >> itemId)
+    {
+        goldRecItems.push_back(itemId);
+    }
+    sort(goldRecItems.begin(), goldRecItems.end());
+
+    vector<itemid_t> results;
+    coVisitManager->getCoVisitation(goldRecItems.size() + 10,
+                                    inputItem, results);
+    sort(results.begin(), results.end());
+
+    BOOST_CHECK_EQUAL_COLLECTIONS(results.begin(), results.end(),
+                                  goldRecItems.begin(), goldRecItems.end());
+}
+
 BOOST_AUTO_TEST_SUITE(VisitManagerTest)
 
 BOOST_AUTO_TEST_CASE(checkVisit)
@@ -76,7 +103,8 @@ BOOST_AUTO_TEST_CASE(checkVisit)
     bfs::remove_all(TEST_DIR_STR);
     bfs::create_directories(TEST_DIR_STR);
 
-    bfs::path visitPath(bfs::path(TEST_DIR_STR) / VISIT_DB_STR);
+    bfs::path visitDBPath(bfs::path(TEST_DIR_STR) / VISIT_DB_STR);
+    bfs::path sessionDBPath(bfs::path(TEST_DIR_STR) / SESSION_DB_STR);
 
     typedef pair<userid_t, itemid_t> VisitPair;
     vector<VisitPair> visitVec;
@@ -104,15 +132,20 @@ BOOST_AUTO_TEST_CASE(checkVisit)
 
     {
         BOOST_TEST_MESSAGE("add visit...");
-        VisitManager visitManager(visitPath.string(), coVisitManager);
+        VisitManager visitManager(visitDBPath.string(), sessionDBPath.string(), coVisitManager);
+        string sessionId("session_001");
         for (vector<VisitPair>::const_iterator it = visitVec.begin();
                 it != visitVec.end(); ++it)
         {
-            BOOST_CHECK(visitManager.addVisitItem(it->first, it->second));
+            BOOST_CHECK(visitManager.addVisitItem(sessionId, it->first, it->second));
         }
 
         checkVisitManager(visitMap, visitManager);
         iterateVisitManager(visitMap, visitManager);
+        checkCoVisitResult(coVisitManager, 10, "20 30");
+        checkCoVisitResult(coVisitManager, 20, "10 30");
+        checkCoVisitResult(coVisitManager, 30, "10 20");
+        checkCoVisitResult(coVisitManager, 40, "");
 
         visitManager.flush();
     }
@@ -120,7 +153,7 @@ BOOST_AUTO_TEST_CASE(checkVisit)
     {
         BOOST_TEST_MESSAGE("continue add visit...");
 
-        VisitManager visitManager(visitPath.string(), coVisitManager);
+        VisitManager visitManager(visitDBPath.string(), sessionDBPath.string(), coVisitManager);
         checkVisitManager(visitMap, visitManager);
         iterateVisitManager(visitMap, visitManager);
 
@@ -128,22 +161,23 @@ BOOST_AUTO_TEST_CASE(checkVisit)
         newVisitVec.push_back(VisitPair(3, 10));
         newVisitVec.push_back(VisitPair(2, 10));
         newVisitVec.push_back(VisitPair(3, 20));
+        newVisitVec.push_back(VisitPair(4, 20));
         newVisitVec.push_back(VisitPair(4, 40));
 
-        // visit duplicate item
-        visitVec.push_back(VisitPair(1, 10));
-        visitVec.push_back(VisitPair(2, 20));
-        visitVec.push_back(VisitPair(3, 30));
-
+        string sessionId("session_002");
         for (vector<VisitPair>::const_iterator it = newVisitVec.begin();
                 it != newVisitVec.end(); ++it)
         {
             visitMap[it->first].insert(it->second);
-            BOOST_CHECK(visitManager.addVisitItem(it->first, it->second));
+            BOOST_CHECK(visitManager.addVisitItem(sessionId, it->first, it->second));
         }
 
         checkVisitManager(visitMap, visitManager);
         iterateVisitManager(visitMap, visitManager);
+        checkCoVisitResult(coVisitManager, 10, "20 30");
+        checkCoVisitResult(coVisitManager, 20, "10 30 40");
+        checkCoVisitResult(coVisitManager, 30, "10 20");
+        checkCoVisitResult(coVisitManager, 40, "20");
 
         visitManager.flush();
     }
