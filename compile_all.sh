4506fb2c9a62f73caf840c406c25400b95e4738f
Jun Jiang <jun.zju@gmail.com>
2011-07-27 16:21:42 +0800

    improve the script compile_all.sh

--- compile_all.sh
+++ compile_all.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-cpu_num=`grep -c "^processor" /proc/cpuinfo`
+CPU_NUM=`grep -c "^processor" /proc/cpuinfo`
 
 cd ..
 CODEBASE_DIR="$(pwd)"
+
+PROJECT_CMAKE_PATH=$CODEBASE_DIR/cmake
+if [ -d $PROJECT_CMAKE_PATH ];then
+  cd $PROJECT_CMAKE_PATH
+  git pull
+else
+  echo "ERROR: $PROJECT_CMAKE_PATH doesn't exist."
+  exit 1
+fi
 
 dependencie=(izenelib icma ijma ilplib imllib idmlib sf1r-engine)
 
@@ -11,19 +20,45 @@ element_count=${#dependencie[@]}
 index=0
 while [ "$index" -lt "$element_count" ]
 do 
-  echo "compile ${dependencie[$index]} ..."
-  if [ -d  $CODEBASE_DIR/${dependencie[$index]}  ];then
-     cd "$CODEBASE_DIR/${dependencie[$index]}/build"
-     git pull
-     make -j$cpu_num
-     if [ ! $? -eq 0 ];then
-       echo "error in compiling ${dependencie[$index]}!"
-       exit 1
-     fi
-  else
-    echo "ERROR:${dependencie[$index]} doesn't exists."
+  echo "build ${dependencie[$index]} ..."
+
+  if [ ! -d $CODEBASE_DIR/${dependencie[$index]} ];then
+    echo "ERROR: ${dependencie[$index]} doesn't exists."
     exit 1
-  fi 
+  fi
+
+  BUILD_PATH=$CODEBASE_DIR/${dependencie[$index]}/build
+  if [ ! -d $BUILD_PATH ];then
+    mkdir $BUILD_PATH
+  fi
+  cd $BUILD_PATH
+
+  git pull
+
+  if [ -f CMakeCache.txt ];then
+    touch CMakeCache.txt
+  else
+    if [ -f $CODEBASE_DIR/${dependencie[$index]}/CMakeLists.txt ];then
+      cmake -DEXTRA_CMAKE_MODULES_DIRS=$PROJECT_CMAKE_PATH ..
+    elif [ -f $CODEBASE_DIR/${dependencie[$index]}/source/CMakeLists.txt ];then 
+      cmake -DEXTRA_CMAKE_MODULES_DIRS=$PROJECT_CMAKE_PATH ../source
+    else
+      echo "ERROR: no CMakeLists.txt found in ${dependencie[$index]}!"
+      exit 1
+    fi
+
+    if [ ! $? -eq 0 ];then
+      echo "ERROR: cmake ${dependencie[$index]}!"
+      exit 1
+    fi
+  fi
+
+  make -j$CPU_NUM
+  if [ ! $? -eq 0 ];then
+    echo "ERROR: compiling ${dependencie[$index]}!"
+    exit 1
+  fi
+
   let "index = $index + 1"
 done
 
