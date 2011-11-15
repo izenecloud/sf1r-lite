b69de418f4584e819fb48a14ccfc664d41e51980
August Njam Grong <longran1989@gmail.com>
2011-11-15 15:03:52 +0800

    utilize PriceHistory in product_manager

--- source/core/product-manager/collection_product_data_source.h
+++ source/core/product-manager/collection_product_data_source.h
@@ -17,26 +17,25 @@ namespace sf1r
 
 class DocumentManager;
 class IndexManager;
-    
+
 class CollectionProductDataSource : public ProductDataSource
 {
 public:
-    
+
     CollectionProductDataSource(const boost::shared_ptr<DocumentManager>& document_manager, const boost::shared_ptr<IndexManager>& index_manager, const boost::shared_ptr<izenelib::ir::idmanager::IDManager>& id_manager, const PMConfig& config, const std::set<PropertyConfig, PropertyComp>& schema);
-    
+
     ~CollectionProductDataSource();
-    
+
     bool GetDocument(uint32_t docid, PMDocumentType& doc);
-    
+
     void GetDocIdList(const izenelib::util::UString& uuid, std::vector<uint32_t>& docid_list, uint32_t exceptid);
-    
+
     bool UpdateUuid(const std::vector<uint32_t>& docid_list, const izenelib::util::UString& uuid);
-    
+
     bool SetUuid(izenelib::ir::indexmanager::IndexerDocument& doc, const izenelib::util::UString& uuid);
-    
+
     bool GetInternalDocidList(const std::vector<izenelib::util::UString>& sdocid_list, std::vector<uint32_t>& docid_list);
-    
-        
+
 private:
     boost::shared_ptr<DocumentManager> document_manager_;
     boost::shared_ptr<IndexManager> index_manager_;
@@ -48,4 +47,3 @@ private:
 }
 
 #endif
-
--- source/core/product-manager/product_data_source.h
+++ source/core/product-manager/product_data_source.h
@@ -16,37 +16,36 @@ namespace sf1r
 class ProductDataSource
 {
 public:
-    
+
     virtual ~ProductDataSource() {}
-    
+
     virtual bool GetDocument(uint32_t docid, PMDocumentType& doc) { return false;}
-    
+
     virtual void GetDocIdList(const izenelib::util::UString& uuid, std::vector<uint32_t>& docid_list, uint32_t exceptid) {}
-    
+
     virtual bool UpdateUuid(const std::vector<uint32_t>& docid_list, const izenelib::util::UString& uuid) {return false;}
-    
+
     virtual bool SetUuid(izenelib::ir::indexmanager::IndexerDocument& doc, const izenelib::util::UString& uuid) {return false;}
-    
+
     virtual bool GetInternalDocidList(const std::vector<izenelib::util::UString>& sdocid_list, std::vector<uint32_t>& docid_list) {return false;}
-    
-   
+
+
     const std::string& GetLastError()
     {
         return error_;
     }
-    
+
 protected:
     void SetError_(const std::string& error)
     {
         error_ = error;
     }
-    
+
 private:
     std::string error_;
-    
+
 };
 
 }
 
 #endif
-
