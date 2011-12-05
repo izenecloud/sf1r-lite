1447f1f053fbcf5b84ac183a6fc20c1ec9d1de2e
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2011-12-05 11:15:14 +0800

    add summarization storage

---
+++ source/core/mining-manager/summarization-submanager/SummarizationStorage.h
@@ -0,0 +1,42 @@
+#ifndef SF1R_MINING_MANAGER_SUMMARIZATION_SUBMANAGER_SUMMARIZATION_STORAGE_H
+#define SF1R_MINING_MANAGER_SUMMARIZATION_SUBMANAGER_SUMMARIZATION_STORAGE_H
+
+#include <am/leveldb/Table.h>
+
+#include "Summarization.h"
+
+#include <boost/thread.hpp>
+
+#include <vector>
+
+namespace sf1r
+{
+
+using izenelib::util::UString;
+
+class SummarizationStorage
+{
+    typedef izenelib::am::leveldb::Table<UString, Summarization > SummarizationDBType;
+
+public:
+    SummarizationStorage(
+            const std::string& dbPath);
+
+    ~SummarizationStorage();
+
+    void Update(const UString& key, const Summarization& value);
+
+    void Flush();
+
+    bool Get(const UString& key, Summarization& result);
+
+    bool IsRebuildSummarizeRequired(
+            const UString& key,
+            const Summarization& value);
+private:
+    SummarizationDBType summarization_db_;
+};
+
+}
+
+#endif
