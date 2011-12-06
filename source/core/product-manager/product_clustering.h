a736ffacb61852c7a5235ad4631bf2f35e6f4964
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2011-12-06 17:26:34 +0800

    fix

--- source/core/product-manager/product_clustering.h
+++ source/core/product-manager/product_clustering.h
@@ -48,7 +48,7 @@ class ProductClustering
 {
 public:
     typedef idmlib::dd::DupDetector<std::string, uint32_t, ProductClusteringAttach> DDType;
-    typedef typename DDType::GroupTableType GroupTableType;
+    typedef DDType::GroupTableType GroupTableType;
     ProductClustering(const std::string& work_dir, const PMConfig& config);
 
     ~ProductClustering();
