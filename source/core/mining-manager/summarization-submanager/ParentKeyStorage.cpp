1ea2a0851c02b00ebc6ad0a65a95aed32fbebffc
August Njam Grong <longran1989@gmail.com>
2011-12-07 21:08:43 +0800

    tiny fix

--- source/core/mining-manager/summarization-submanager/ParentKeyStorage.cpp
+++ source/core/mining-manager/summarization-submanager/ParentKeyStorage.cpp
@@ -34,6 +34,7 @@ ParentKeyStorage::~ParentKeyStorage()
 
 void ParentKeyStorage::AppendUpdate(const UString& parent, const UString& child)
 {
+    child_to_parent_db_.update(child, parent);
     buffer_db_[parent].push_back(child);
 
     ++buffer_size_;
@@ -43,6 +44,7 @@ void ParentKeyStorage::AppendUpdate(const UString& parent, const UString& child)
 
 void ParentKeyStorage::Flush()
 {
+    child_to_parent_db_.flush();
     if (buffer_db_.empty()) return;
 
     BufferType::iterator it = buffer_db_.begin();
@@ -50,19 +52,11 @@ void ParentKeyStorage::Flush()
     {
         std::vector<UString> v;
         parent_to_children_db_.get(it->first, v);
-
-        for (std::vector<UString>::iterator vit = it->second.begin();
-                vit != it->second.end(); ++vit)
-        {
-            child_to_parent_db_.update(*vit, it->first);
-            v.push_back(UString());
-            v.back().swap(*vit);
-        }
+        v.insert(v.end(), it->second.begin(), it->second.end());
         parent_to_children_db_.update(it->first, v);
     }
 
     parent_to_children_db_.flush();
-    child_to_parent_db_.flush();
     buffer_db_.clear();
     buffer_size_ = 0;
 }
