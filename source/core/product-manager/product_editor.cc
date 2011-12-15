43338d1fc26c3d0cb32643b1f80abe5377fcf947
Jia Guo <guojia@gmail.com>
2011-12-15 14:25:18 +0800

    add product removal edit function.

--- source/core/product-manager/product_editor.cc
+++ source/core/product-manager/product_editor.cc
@@ -284,6 +284,115 @@ bool ProductEditor::AppendToGroup_(const std::vector<PMDocumentType>& doc_list, 
 
 bool ProductEditor::RemoveFromGroup(const izenelib::util::UString& uuid, const std::vector<uint32_t>& docid_list, const ProductEditOption& option)
 {
-    error_ = "Remove not supported yet";
-    return false;
+    {
+        std::vector<uint32_t> same_docid_list;
+        data_source_->GetDocIdList(uuid, same_docid_list, 0);
+        if (same_docid_list.empty())
+        {
+            std::string suuid;
+            uuid.convertString(suuid, izenelib::util::UString::UTF_8);
+            error_ = "UUID "+suuid+" was not exists";
+            return false;
+        }
+    }
+    std::vector<PMDocumentType> doc_list;
+    doc_list.reserve(docid_list.size());
+    for (uint32_t i = 0; i < docid_list.size(); i++)
+    {
+        PMDocumentType doc;
+        if (!data_source_->GetDocument(docid_list[i], doc))
+        {
+            if( !option.force )
+            {
+                error_ = "Can not get document "+boost::lexical_cast<std::string>(docid_list[i]);
+                return false;
+            }
+            else
+            {
+                LOG(WARNING)<<"Can not get document "<<docid_list[i]<<std::endl;
+            }
+        }
+        else
+        {
+            doc_list.push_back(doc);
+        }
+    }
+    if(doc_list.empty())
+    {
+        if( !option.force )
+        {
+            error_ = "Empty document list.";
+            return false;
+        }
+        else
+        {
+            LOG(WARNING)<<"Empty document list."<<std::endl;
+            return true;
+        }
+    }
+    std::vector<UString> uuid_list(doc_list.size());
+    for (uint32_t i = 0; i < doc_list.size(); i++)
+    {
+        uint32_t docid = doc_list[i].getId();
+        if (!doc_list[i].getProperty(config_.uuid_property_name, uuid_list[i]))
+        {
+            error_ = "Can not get uuid in document "+boost::lexical_cast<std::string>(docid);
+            return false;
+        }
+        if( uuid_list[i] != uuid )
+        {
+            error_ = "Uuid in document "+boost::lexical_cast<std::string>(docid)+" does not match the request uuid";
+            return false;
+        }
+    }
+    
+    for (uint32_t i = 0; i < doc_list.size(); i++)
+    {
+        uint32_t docid = doc_list[i].getId();
+        PMDocumentType new_doc(doc_list[i]);
+        izenelib::util::UString doc_uuid;
+        UuidGenerator::Gen(doc_uuid);
+        new_doc.property(config_.docid_property_name) = doc_uuid;
+        new_doc.eraseProperty(config_.uuid_property_name);
+        util_.SetItemCount(new_doc, 1);
+#ifdef PM_EDIT_INFO
+        LOG(INFO)<<"Output : "<<1<<" , "<<doc_uuid<<" , itemcount: "<<1<<std::endl;
+#endif
+        op_processor_->Append(1, new_doc);
+        std::vector<uint32_t> update_docid_list(1, docid);
+#ifdef PM_EDIT_INFO
+        LOG(INFO)<<"Updating uuid : "<<docid<<" , "<<doc_uuid<<std::endl;
+#endif
+        if (!data_source_->UpdateUuid(update_docid_list, doc_uuid))
+        {
+            error_ = "Update uuid failed";
+            return false;
+        }
+    }
+    data_source_->Flush();
+    PMDocumentType origin_doc;
+    origin_doc.property(config_.docid_property_name) = uuid;
+    std::vector<uint32_t> same_docid_list;
+    data_source_->GetDocIdList(uuid, same_docid_list, 0);
+    if (same_docid_list.empty())
+    {
+        //all deleted
+#ifdef PM_EDIT_INFO
+        LOG(INFO)<<"Output : "<<3<<" , "<<uuid<<std::endl;
+#endif
+        op_processor_->Append(3, origin_doc);
+    }
+    else
+    {
+        ProductPrice price;
+        util_.GetPrice(same_docid_list, price);
+        origin_doc.property(config_.price_property_name) = price.ToUString();
+        uint32_t itemcount = same_docid_list.size();
+        util_.SetItemCount(origin_doc, itemcount);
+#ifdef PM_EDIT_INFO
+        LOG(INFO)<<"Output : "<<2<<" , "<<uuid<<" , itemcount: "<<itemcount<<std::endl;
+#endif
+        op_processor_->Append(2, origin_doc);
+    }
+    return true;
 }
