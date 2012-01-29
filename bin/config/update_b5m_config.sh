6401d1defe677af42fc412e1b6a87d7d4981ae06
Jia Guo <guojia@gmail.com>
2012-01-29 17:32:45 +0800

    add collection 'second' config

--- bin/config/update_b5m_config.sh
+++ bin/config/update_b5m_config.sh
@@ -5,3 +5,4 @@ scp dev@izenesoft.cn:/data/B5M/config/b5ma.xml $HOM/
 scp dev@izenesoft.cn:/data/B5M/config/b5mc.xml $HOM/
 scp dev@izenesoft.cn:/data/B5M/config/tuanm.xml $HOM/
 scp dev@izenesoft.cn:/data/B5M/config/tuana.xml $HOM/
+scp dev@izenesoft.cn:/data/B5M/config/second.xml $HOM/
