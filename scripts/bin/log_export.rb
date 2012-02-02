4b6461adbdbf0199909e00469627c25c5aee4993
Jia Guo <guojia@gmail.com>
2012-02-02 15:48:44 +0800

    add cclog conversion in log_export

--- scripts/bin/log_export.rb
+++ scripts/bin/log_export.rb
@@ -7,17 +7,20 @@ require_rel '../lib/call_histroy_exporter'
 
 #ip = "180.153.140.110"
 ip = ARGV[0]
-history_file = ARGV[1]
-# output_file = ARGV[1]
-
+cc_file = ARGV[1]
+PREFIX = "INFO  com.izenesoft.agent.plugin.sf1.Sf1Client  - Send JSON request: "
 api = Sf1rApi.new(ip)
-in_file = File.open(history_file, "r")
-# out_file = File.new(output_file, "w")
+in_file = File.open(cc_file, "r")
 exporter = CallHistroyExporter.new(api)
 in_file.each_with_index do |line, i|
   
   $stderr.puts "Processing line #{i}" if i%10==0
   line.strip!
+  if line =~ /#{PREFIX}(.+) At: /
+    line = $~[1]
+  else
+    line = ""
+  end
   next if line.empty?
   request = JSON.parse(line)
   collection = request['collection']
@@ -28,14 +31,12 @@ in_file.each_with_index do |line, i|
   next if crequest==nil
   if crequest.class == [].class
     crequest.each do |c|
-#       out_file.puts c.to_json
       puts c.to_json
     end
   else
-#     out_file.puts crequest.to_json
     puts crequest.to_json
   end
   
 end
 in_file.close
-# out_file.close
+
