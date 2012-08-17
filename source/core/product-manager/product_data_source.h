44a14286e44cdf78fb3daad39c7830a598218594
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2012-08-17 20:45:39 +0800

    fix bug in commit 0613b31e347707d8d924ffd5a052ceacdefa5901 : It will lead to empty generated SCD from tuanm

--- source/core/product-manager/product_data_source.h
+++ source/core/product-manager/product_data_source.h
@@ -21,7 +21,7 @@ public:
 
     virtual uint32_t GetMaxDocId() const { return 0; }
 
-    virtual bool GetDocument(uint32_t docid, PMDocumentType& doc) { return false; }
+    virtual bool GetDocument(uint32_t docid, PMDocumentType& doc, bool force = false) { return false; }
 
     virtual void GetRTypePropertiesForDocument(uint32_t docid, PMDocumentType& doc) {}
 
