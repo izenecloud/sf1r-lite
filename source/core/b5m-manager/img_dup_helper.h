190b76d5c11549ba70ea6ff3483f2a9c7c4690ba
Lucas.liu <Lucas.liu@b5m.com>
2012-12-25 17:02:29 +0800

    GetPsmItem is changed

--- source/core/b5m-manager/img_dup_helper.h
+++ source/core/b5m-manager/img_dup_helper.h
@@ -57,7 +57,7 @@ namespace sf1r {
             uint32_t i;
             for(i=0;i<img_url.size()-2;i+=2)
             {
-                doc_vector.push_back(make_pair(img_url.substr(i,3), 0.4));
+                doc_vector.push_back(make_pair(img_url.substr(i,3), 0.5));
             }
             for(i=0;i<img_url.size()-5;i+=5)
             {
@@ -65,9 +65,9 @@ namespace sf1r {
             }
             for(i=0;i<img_url.size()-8;i+=8)
             {
-                doc_vector.push_back(make_pair(img_url.substr(i,9), 1.0));
+                doc_vector.push_back(make_pair(img_url.substr(i,9), 0.9));
             }
-
+            doc_vector.push_back(make_pair(img_url, 1.0));
             return true;
         }
 
