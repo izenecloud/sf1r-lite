6dd323e1e7a0c605ec4cfceb2c72d962b61221a6
Vincent Lee <vincent.lee@b5m.com>
2012-12-28 18:12:04 +0800

    add local sf1r driver start script

--- source/process/distribute/cgi/start_sf1r_cgi.sh
+++ source/process/distribute/cgi/start_sf1r_cgi.sh
@@ -1,3 +1,3 @@
 for i in $(seq 1 10); do
-    spawn-fcgi -s "/tmp/fcgi/sf1r$i.socket" -f "./sf1r_process.fcgi 180.153.140.110:2181,180.153.140.111:2181,180.153.140.112:2181  distributed beta >> /tmp/fcgi/sf1r$i.log 2>&1" -P ./sf1r_process.fcgi.pid -F 2
+    spawn-fcgi -s "/tmp/fcgi/sf1r$i.socket" -f "./sf1r_process.fcgi 180.153.140.110:2181,180.153.140.111:2181,180.153.140.112:2181  distributed beta >> /tmp/fcgi/sf1r$i.log 2>&1" -P ./sf1r_process.fcgi.pid -F 1
 done
