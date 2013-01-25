7fa7b47522e15ed3a0c7fa018072cba51ab535e8
August Njam Grong <longran1989@gmail.com>
2013-01-25 21:57:31 +0800

    fix uninitialized member warning

--- source/core/b5m-manager/img_dup_helper.h
+++ source/core/b5m-manager/img_dup_helper.h
@@ -13,72 +13,75 @@
 
 namespace sf1r {
 
-    struct PsmAttach
+struct PsmAttach
+{
+    friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version)
     {
+    }
 
-        uint8_t id;
-        friend class boost::serialization::access;
-        template<class Archive>
-        void serialize(Archive & ar, const unsigned int version)
-        {
-            ar & id;
-        }
-
-        bool dd(const PsmAttach& other) const
-        {
-            return true;
-        }
-
-        friend std::ostream& operator<<(std::ostream& out, const PsmAttach& attach)
-        {
-            out<<"id "<<attach.id;
-            return out;
-        }
-
-    };
-
-    class PsmHelper
+    bool dd(const PsmAttach& other) const
     {
-        public:
-            static bool GetPsmItem(ProductTermAnalyzer& analyzer
-                    , std::map<std::string, izenelib::util::UString>& doc
-                    , std::string& key, std::vector<std::pair<std::string, double> >& doc_vector, PsmAttach& attach)
-            {
-                if(doc["Img"].length()==0)
-                {
-                    LOG(INFO)<<"There is no Img ... "<<std::endl;
-                    return false;
-                }
-                doc["DOCID"].convertString(key, izenelib::util::UString::UTF_8);
-                std::string img_url;
-                doc["Img"].convertString(img_url, izenelib::util::UString::UTF_8);
-                analyzer.Analyze(doc["Img"].substr(7), doc_vector);
-                uint32_t i;
-                uint32_t size = img_url.size();
-                if(size < 60)
-                {
-                    for(i=20;i<size-6;i++)
-                    {
-                        doc_vector.push_back(make_pair(img_url.substr(i, 7), 1.0));
-                    }
-                }
-                return true;
-            }
+        return true;
+    }
 
-            static bool GetPsmItemCon(ProductTermAnalyzer& analyzer
-                    , std::map<std::string, izenelib::util::UString>& doc
-                    , std::string& key, std::vector<std::pair<std::string, double> >& doc_vector, PsmAttach& attach, uint32_t length)
+    friend std::ostream& operator<<(std::ostream& out, const PsmAttach& attach)
+    {
+        return out;
+    }
+};
+
+class PsmHelper
+{
+public:
+    static bool GetPsmItem(
+            ProductTermAnalyzer& analyzer,
+            std::map<std::string, izenelib::util::UString>& doc,
+            std::string& key,
+            std::vector<std::pair<std::string, double> >& doc_vector,
+            PsmAttach& attach)
+    {
+        const izenelib::util::UString& img_ustr = doc["Img"];
+        if (img_ustr.empty())
+        {
+            LOG(INFO) << "There is no Img ... " << std::endl;
+            return false;
+        }
+        doc["DOCID"].convertString(key, izenelib::util::UString::UTF_8);
+        std::string img_url;
+        img_ustr.convertString(img_url, izenelib::util::UString::UTF_8);
+        analyzer.Analyze(img_ustr.substr(7), doc_vector);
+        uint32_t i;
+        uint32_t size = img_url.size();
+        if (size < 60)
+        {
+            for (i = 20; i < size - 6; ++i)
             {
-                if(doc["Content"].length()<=length)
-                {
-//                    LOG(INFO)<<"Content Empty... "<<std::endl;
-                    return false;
-                }
-                doc["DOCID"].convertString(key, izenelib::util::UString::UTF_8);
-                analyzer.Analyze(doc["Content"], doc_vector);
-                return true;
+                doc_vector.push_back(std::make_pair(img_url.substr(i, 7), 1.0));
             }
-    };
+        }
+        return true;
+    }
+
+    static bool GetPsmItemCon(
+            ProductTermAnalyzer& analyzer,
+            std::map<std::string, izenelib::util::UString>& doc,
+            std::string& key,
+            std::vector<std::pair<std::string, double> >& doc_vector,
+            PsmAttach& attach,
+            uint32_t length)
+    {
+        if (doc["Content"].length() <= length)
+        {
+//          LOG(INFO) << "Content Empty...";
+            return false;
+        }
+        doc["DOCID"].convertString(key, izenelib::util::UString::UTF_8);
+        analyzer.Analyze(doc["Content"], doc_vector);
+        return true;
+    }
+};
 
 }
 
