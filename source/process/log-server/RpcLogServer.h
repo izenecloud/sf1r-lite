051c6bee2026993a7cc9ab96f677bcb7d2ae23ea
Lucas.liu <Lucas.liu@b5m.com>
2013-03-22 12:11:58 +0800

    dispatch user query req

--- source/process/log-server/RpcLogServer.h
+++ source/process/log-server/RpcLogServer.h
@@ -46,6 +46,10 @@ public:
     void GetOldUUID(OldUUIDData& reqdata);
     void GetOldDocId(OldDocIdData& reqdata);
 
+    void GetFreqUserQueries(GetFreqUserQueriesData & reqdata,
+            std::list< std::map<std::string, std::string> > & results);
+    void InjectUserQuery(InjectUserQueryData & reqdata,
+            bool & result);
     void synchronize(const SynchronizeData& syncReqData);
 
     /// Will be called when update is actually performed
