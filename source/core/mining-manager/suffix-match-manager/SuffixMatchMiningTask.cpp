2f7ec387756d6ecf7c01e05f1d49eb422b01c314
Hongliang <hongliang.zhao@b5m.com>
2013-03-26 17:15:46 +0800

    tiny fix

--- source/core/mining-manager/suffix-match-manager/SuffixMatchMiningTask.cpp
+++ source/core/mining-manager/suffix-match-manager/SuffixMatchMiningTask.cpp
@@ -68,32 +68,29 @@ bool SuffixMatchMiningTask::preProcess()
         if (!need_rebuild)
         {
             /*
-            @brief : in here document_manager_->isThereRtypeDoc() just means for the -R SCD, 
+            @brief : in here document_manager_->isThereRtypePro() just means for the -R SCD, 
             because the Rtype doc in -U SCD is not in this flow.
             */
-            if (document_manager_->isThereRtypeDoc()) 
+            if (document_manager_->isThereRtypePro())
             {
                 LOG (INFO) << "Update for R-type SCD" << endl;
                 const std::vector<std::pair<int32_t, std::string> >& prop_list = filter_manager_->getProp_list();
-                uint32_t count = 0;
                 for (std::vector<std::pair<int32_t, std::string> >::const_iterator i = prop_list.begin(); i != prop_list.end(); ++i)
                 {
                     std::set<string>::iterator iter = document_manager_->RtypeDocidPros_.find((*i).second);
                     if (iter == document_manager_->RtypeDocidPros_.end())
                     {
-                        count++;
                         if (!filter_manager_->isNumericProp((*i).second))
                         {
                             new_filter_manager->addUnchangedProperty((*i).second);
                         }
                     }
                 }
-                if (count == prop_list.size())// means no filter property need rebuild, so return;
-                {
-                    LOG (INFO) << "All the filter properties do not need rebuild ..." << endl;
-                    return false;
-                }
-
+            }
+            else
+            {
+                LOG (INFO) << "All filter properties do not need rebuild ... " << endl;
+                return false;
             }
         }
     }
