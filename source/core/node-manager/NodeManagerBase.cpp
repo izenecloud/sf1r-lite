6e467b02b83dec186de2df155b78a865a3668fd7
B5M-DATA6 <ops@B5M-DATA6>
2013-04-19 00:09:42 +0800

    tiny

--- source/core/node-manager/NodeManagerBase.cpp
+++ source/core/node-manager/NodeManagerBase.cpp
@@ -1417,6 +1417,8 @@ void NodeManagerBase::checkForPrimaryElecting()
 bool NodeManagerBase::checkElectingInAsyncMode(uint32_t last_reqid)
 {
     boost::unique_lock<boost::mutex> lock(mutex_);
+    if (!zookeeper_ || !zookeeper_->isConnected())
+        return false;
     updateCurrentPrimary();
     if (getPrimaryState() == NODE_STATE_ELECTING || need_check_electing_)
     {
