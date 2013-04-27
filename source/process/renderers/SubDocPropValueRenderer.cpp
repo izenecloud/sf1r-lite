2cc19566c174b13a1fedc406e46b49b6e686e50e
kevin <kevin.lin@b5m.com>
2013-04-27 08:24:18 +0800

    fix bug of SubDocPropValueRenderer

--- source/process/renderers/SubDocPropValueRenderer.cpp
+++ source/process/renderers/SubDocPropValueRenderer.cpp
@@ -1,5 +1,6 @@
 #include "SubDocPropValueRenderer.h"
 #include <rapidjson/document.h>
+#include <glog/logging.h>
 
 namespace sf1r
 {
@@ -8,20 +9,22 @@ void SubDocPropValueRenderer::renderSubDocPropValue(
         const std::string& origText,
         izenelib::driver::Value& resourceValue)
 {
-	rapidjson::Document doc;
-	doc.Parse<0>(origText.c_str());
-	assert(doc.IsArray());
-	const rapidjson::Value& subDocs = doc;
-	assert(subDocs.IsArray());
-	for (rapidjson::Value::ConstValueIterator vit = subDocs.Begin();
-	     vit != subDocs.End(); vit++)
-	{
+    if (origText.empty())
+        return;
+    rapidjson::Document doc;
+    doc.Parse<0>(origText.c_str());
+    const rapidjson::Value& subDocs = doc;
+    assert(subDocs.IsArray());
+    for (rapidjson::Value::ConstValueIterator vit = subDocs.Begin();
+         vit != subDocs.End(); vit++)
+    {
 	    izenelib::driver::Value& subDoc = resourceValue();
-	    for (rapidjson::Value::ConstMemberIterator mit = vit->MemberBegin();		      
+        for (rapidjson::Value::ConstMemberIterator mit = vit->MemberBegin();		      
 		     mit != vit->MemberEnd(); mit++)
-	    {
+        {
+            assert(mit->IsObject());
 		    subDoc[mit->name.GetString()]=mit->value.GetString();
-		}
+        }
     }
 }
 }
