izenecloud/sf1r-lite

commit f3d128f2152bde8e7f96892661a4b66fff79600f

modify SearchWorker.cpp to correct Highlight

source/core/aggregator-manager/SearchWorker.cpp
@@ -691,7 +691,8 @@ bool  SearchWorker::getResultItem(
             actionItem.languageAnalyzerInfo_.useOriginalKeyword_
     );
 
-    //analyze_(actionItem.env_.queryString_, queryTerms);
+    //queryTerms is begin segmented after analyze_()
+    analyze_(actionItem.env_.queryString_, queryTerms, true);
 
     // propertyOption
     if (!actionItem.env_.taxonomyLabel_.empty())

commit 88aea5a23415b2da1b2e42c7c964381a9a88e091

fix smoke test failure on snippet/highlighter caused by commit f3d128f2152bde8e7f96892661a4b66fff79600f

source/core/aggregator-manager/SearchWorker.cpp
@@ -564,13 +564,12 @@ bool SearchWorker::getSummaryMiningResult_(
 
 void SearchWorker::analyze_(const std::string& qstr, std::vector<izenelib::util::UString>& results, bool isQA)
 {
-    results.clear();
-    izenelib::util::UString question(qstr, izenelib::util::UString::UTF_8);
-    la::TermList termList;
-
     la::LA* pLA = LAPool::getInstance()->popSearchLA( analysisInfo_);
 //    pLA->process_search(question, termList);
     if (!pLA) return;
+    results.clear();
+    izenelib::util::UString question(qstr, izenelib::util::UString::UTF_8);
+    la::TermList termList;
     pLA->process(question, termList);
     LAPool::getInstance()->pushSearchLA( analysisInfo_, pLA );
 