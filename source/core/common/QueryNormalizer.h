689a37596d34e7fb32e7164737c98412754948d5
Jun Jiang <jun.jiang@izenesoft.com>
2013-06-09 14:18:57 +0800

    Change QueryNormalizer to a singleton class.

--- source/core/common/QueryNormalizer.h
+++ source/core/common/QueryNormalizer.h
@@ -1,13 +1,12 @@
 /**
  * @file QueryNormalizer.h
- * @brief normalize the query string for the cases such as below:
- * 1. mapping from query to label
- * 2. mapping from query to custom rank
+ * @brief This class is used to normalize the query string (in UTF-8 encoding).
  */
 
 #ifndef SF1R_QUERY_NORMALIZER_H
 #define SF1R_QUERY_NORMALIZER_H
 
+#include <util/singleton.h>
 #include <string>
 
 namespace sf1r
@@ -16,10 +15,15 @@ namespace sf1r
 class QueryNormalizer
 {
 public:
+    static QueryNormalizer* get()
+    {
+        return izenelib::util::Singleton<QueryNormalizer>::get();
+    }
+
     /*
-     * Normalize the query (in UTF-8 encoding) from @p fromStr to @p toStr.
+     * Normalize the query from @p fromStr to @p toStr.
      *
-     * The normalization consists of below steps:
+     * It consists of below steps:
      * 1. extract the tokens using white space as delimiter
      * 2. convert the alphabets in tokens to lower case
      * 3. sort the tokens in lexicographical order
@@ -28,7 +32,7 @@ public:
      * For example, given the query "三星 Galaxy I9300",
      * it would be normalized to "galaxy i9300 三星".
      */
-    static void normalize(const std::string& fromStr, std::string& toStr);
+    void normalize(const std::string& fromStr, std::string& toStr);
 };
 
 } // namespace sf1r
