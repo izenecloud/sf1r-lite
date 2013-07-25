7278712be7d0596b85aec56604fcb2255458abc1
Jun Jiang <jun.jiang@izenesoft.com>
2013-07-25 10:41:52 +0800

    Previously, fuzzy search does not support excluding docids in custom rank, now it's revised to support this feature.

--- source/core/search-manager/FuzzySearchRanker.cpp
+++ source/core/search-manager/FuzzySearchRanker.cpp
@@ -3,12 +3,15 @@
 #include "CustomRanker.h"
 #include "Sorter.h"
 #include "HitQueue.h"
-#include "ScoreDocEvaluator.h"
 
 #include <configuration-manager/ProductRankingConfig.h>
 #include <query-manager/SearchKeywordOperation.h>
+#include <query-manager/ActionItem.h>
 #include <common/PropSharedLockSet.h>
+#include <mining-manager/product-scorer/ProductScorer.h>
+#include "mining-manager/custom-rank-manager/CustomRankManager.h"
 
+#include <algorithm>
 #include <boost/shared_ptr.hpp>
 #include <boost/scoped_ptr.hpp>
 
@@ -17,6 +20,7 @@ using namespace sf1r;
 FuzzySearchRanker::FuzzySearchRanker(SearchManagerPreProcessor& preprocessor)
     : preprocessor_(preprocessor)
     , fuzzyScoreWeight_(0)
+    , customRankManager_(NULL)
 {
 }
 
@@ -26,7 +30,67 @@ void FuzzySearchRanker::setFuzzyScoreWeight(const ProductRankingConfig& rankConf
     fuzzyScoreWeight_ = fuzzyConfig.weight;
 }
 
-void FuzzySearchRanker::rank(
+void FuzzySearchRanker::rankByProductScore(
+    const KeywordSearchActionItem& actionItem,
+    std::vector<ScoreDocId>& resultList)
+{
+    PropSharedLockSet propSharedLockSet;
+    boost::scoped_ptr<ProductScorer> productScorer(
+        preprocessor_.createProductScorer(actionItem, propSharedLockSet, NULL));
+
+    if (!productScorer)
+        return;
+
+    std::set<docid_t> excludeDocIds;
+    getExcludeDocIds_(actionItem.env_.normalizedQueryString_,
+                      excludeDocIds);
+
+    const std::size_t count = resultList.size();
+    std::size_t current = 0;
+    for (std::size_t i = 0; i < count; ++i)
+    {
+        docid_t docId = resultList[i].second;
+
+        // ignore the exclude docids
+        if (excludeDocIds.find(docId) != excludeDocIds.end())
+            continue;
+
+        double fuzzyScore = resultList[i].first * fuzzyScoreWeight_;
+        double productScore = productScorer->score(docId);
+
+        resultList[i].first = fuzzyScore + productScore;
+        resultList[current++] = resultList[i];
+    }
+
+    resultList.resize(current);
+    std::sort(resultList.begin(), resultList.end(), std::greater<ScoreDocId>());
+
+    if (current == count)
+    {
+        LOG(INFO) << "rank by product score, topk count not changed: " << count;
+    }
+    else
+    {
+        LOG(INFO) << "rank by product score, topk count changed from "
+                  << count << " to " << current;
+    }
+}
+
+void FuzzySearchRanker::getExcludeDocIds_(const std::string& query,
+                                          std::set<docid_t>& excludeDocIds)
+{
+    if (customRankManager_ == NULL)
+        return;
+
+    CustomRankDocId customDocId;
+    if (customRankManager_->getCustomValue(query, customDocId))
+    {
+        excludeDocIds.insert(customDocId.excludeIds.begin(),
+                             customDocId.excludeIds.end());
+    }
+}
+
+void FuzzySearchRanker::rankByPropValue(
         const SearchKeywordOperation& actionOperation,
         uint32_t start,
         std::vector<uint32_t>& docid_list,
@@ -49,20 +113,15 @@ void FuzzySearchRanker::rank(
         return;
     }
 
-    PropSharedLockSet propSharedLockSet;
-    ProductScorer* productScorer = preprocessor_.createProductScorer(
-        actionOperation.actionItem_, propSharedLockSet, NULL);
-
-    if (productScorer == NULL && !customRanker &&
+    if (!customRanker &&
         preprocessor_.isSortByRankProp(actionOperation.actionItem_.sortPriorityList_))
     {
         LOG(INFO) << "no need to resort, sorting by original fuzzy match order.";
         return;
     }
 
-    ScoreDocEvaluator scoreDocEvaluator(productScorer, customRanker);
     const std::size_t count = docid_list.size();
-
+    PropSharedLockSet propSharedLockSet;
     boost::scoped_ptr<HitQueue> scoreItemQueue;
     if (pSorter)
     {
@@ -80,16 +139,11 @@ void FuzzySearchRanker::rank(
     for (size_t i = 0; i < count; ++i)
     {
         tmpdoc.docId = docid_list[i];
-        scoreDocEvaluator.evaluate(tmpdoc);
+        tmpdoc.score = result_score_list[i];
 
-        float fuzzyScore = result_score_list[i];
-        if (!productScorer)
+        if (customRanker)
         {
-            tmpdoc.score = fuzzyScore;
-        }
-        else
-        {
-            tmpdoc.score += fuzzyScore * fuzzyScoreWeight_;
+            tmpdoc.custom_score = customRanker->evaluate(tmpdoc.docId);
         }
 
         scoreItemQueue->insert(tmpdoc);
