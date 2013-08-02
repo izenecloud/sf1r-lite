bfebb0cc2e3a7ee4796a45557f48d30656d1f26f
Kevin Lin <kevin.lin@b5m.com>
2013-08-02 08:04:40 +0800

    Condition for *

--- source/core/mining-manager/query-intent/QueryIntentHelper.cpp
+++ source/core/mining-manager/query-intent/QueryIntentHelper.cpp
@@ -15,14 +15,10 @@ using driver::Keys;
 using namespace izenelib::driver;
 using namespace NQI;
 
-void refineRequest(izenelib::driver::Request& request,
-             izenelib::driver::Response& response,
+static void refineGroupLabels(izenelib::driver::Value& groupLabels,
+             izenelib::driver::Value& queryIntents,
              WMVContainer& wmvs)
 {
-    if (wmvs.empty())
-        return;
-    izenelib::driver::Value& queryIntents = response["query_intent"];
-    izenelib::driver::Value& groupLabels = request[Keys::search][Keys::group_label];
     WMVCIterator array;
     WMVIterator item;
     for (array = wmvs.begin(); array != wmvs.end(); array++)
@@ -66,6 +62,58 @@ void refineRequest(izenelib::driver::Request& request,
             }
             queryIntentValues() = item->first;
         }
+    }
+}
+
+static void refineConditions(izenelib::driver::Value& conditions,
+             izenelib::driver::Value& queryIntents,
+             WMVContainer& wmvs)
+{
+    WMVCIterator array;
+    WMVIterator item;
+    for (array = wmvs.begin(); array != wmvs.end(); array++)
+    {
+        if (array->second.empty())
+            continue;
+        izenelib::driver::Value& condition = conditions();
+        condition[Keys::property] = array->first.name_;
+        condition["operator"] = array->first.op_;
+        int operands = array->first.operands_;
+        if (-1 == operands)
+            operands = std::numeric_limits<int>::max();
+        izenelib::driver::Value& values = condition[Keys::value];
+        izenelib::driver::Value& queryIntent = queryIntents();
+        izenelib::driver::Value& queryIntentValues = queryIntent[array->first.name_];
+        int i = 0;
+        for (item = array->second.begin(); item != array->second.end(); i++, item++)
+        {
+            if (i >= operands)
+                break;
+            values() = item->first;
+            queryIntentValues() = item->first;
+            LOG(INFO)<<item->first;
+        }
+    }
+}
+
+void refineRequest(izenelib::driver::Request& request,
+             izenelib::driver::Response& response,
+             WMVContainer& wmvs)
+{
+    if (wmvs.empty())
+        return;
+    izenelib::driver::Value& queryIntents = response["query_intent"];
+    //if ("suffix" == request[Keys::search][Keys::searching_mode][Keys::mode])
+    if (("*" == request[Keys::search][Keys::keywords]) && 
+        ("suffix" != request[Keys::search][Keys::searching_mode][Keys::mode]))
+    {
+        izenelib::driver::Value& conditions = request[Keys::conditions];
+        refineConditions(conditions, queryIntents, wmvs);
+    }
+    else
+    {
+        izenelib::driver::Value& groupLabels = request[Keys::search][Keys::group_label];
+        refineGroupLabels(groupLabels, queryIntents, wmvs);
     }
 }
 
