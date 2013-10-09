18206127f092b67f6ef1884fdd93210b18d8e9ae
Vincent Lee <vincent.lee@b5m.com>
2013-10-09 16:49:38 +0800

    tiny fix

--- source/bundles/index/IndexBundleActivator.cpp
+++ source/bundles/index/IndexBundleActivator.cpp
@@ -552,7 +552,8 @@ IndexBundleActivator::createSearchWorker_()
 boost::shared_ptr<SearchAggregator>
 IndexBundleActivator::createSearchAggregator_(bool readonly)
 {
-    searchMerger_.reset(new SearchMerger());
+    if (!searchMerger_)
+        searchMerger_.reset(new SearchMerger());
 
     std::auto_ptr<SearchMergerProxy> mergerProxy(new SearchMergerProxy(searchMerger_.get()));
     searchMerger_->bindCallProxy(*mergerProxy);
