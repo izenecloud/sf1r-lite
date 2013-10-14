d1ec63119209b521991c05e3fec7519e91af5415
lucas.liu <lucas.liu@b5m.com>
2013-10-14 10:53:31 +0800

    be_index has been modified, so i have to fix some bugs

--- source/core/mining-manager/ad-index-manager/AdMiningTask.cpp
+++ source/core/mining-manager/ad-index-manager/AdMiningTask.cpp
@@ -6,6 +6,7 @@
 
 namespace sf1r
 {
+
 AdMiningTask::AdMiningTask(
         const std::string& path,
         boost::shared_ptr<DocumentManager> dm)
@@ -13,27 +14,75 @@ AdMiningTask::AdMiningTask(
 {
     adIndex_.reset(new AdIndexType());
 }
+
 AdMiningTask::~AdMiningTask()
 {
 }
 
 bool AdMiningTask::buildDocument(docid_t docID, const Document& doc)
 {
+    std::string sDnf;
+    doc.getProperty("DNF", sDnf);
+    DNF dnf;
+    sf1r::DNFParser::parseDNF(sDnf, dnf);
+    adIndex_->addDNF(docID, dnf);
     return true;
 }
+
 bool AdMiningTask::preProcess(int64_t timestamp)
 {
-    return true;
+    startDocId_ = adIndex_->totalNumDNF()+1;
+    const docid_t endDocId = documentManager_->getMaxDocId();
+
+    LOG(INFO) << "AdIndex mining task"
+              << ", start docid: " << startDocId_
+              << ", end docid: " << endDocId <<endl;;
+    return startDocId_ <= endDocId;
 }
+
 bool AdMiningTask::postProcess()
 {
+    std::ofstream ofs(indexPath_.c_str(), std::ios_base::binary);
+    if(!ofs)
+    {
+        LOG(INFO) << "failed openning file " << indexPath_<<endl;
+        return false;
+    }
+
+    try
+    {
+        adIndex_->save(ofs);
+    }
+    catch(const std::exception& e)
+    {
+        LOG(ERROR) << "exception in writing file "<<e.what()
+                   << ", path: " <<indexPath_<<endl;
+        return false;
+    }
     return true;
 }
+
 void AdMiningTask::save()
 {
+    postProcess();
 }
+
 bool AdMiningTask::load()
 {
+    std::ifstream ifs(indexPath_.c_str(), std::ios_base::binary);
+    if(!ifs)
+        return false;
+    try
+    {
+        adIndex_->load(ifs);
+    }
+    catch(const std::exception& e)
+    {
+        LOG(ERROR) << "exception in read file: " << e.what()
+                   << ", path: "<< indexPath_<<endl;
+        return false;
+    }
     return true;
 }
+
 } //namespace sf1r
--- source/core/mining-manager/ad-index-manager/DNFParser.h
+++ source/core/mining-manager/ad-index-manager/DNFParser.h
@@ -4,11 +4,13 @@
 #ifndef SF1_AD_INDEX_DNFPARSER_H_
 #define SF1_AD_INDEX_DNFPARSER_H_
 
+#include <ir/be_index/DNF.hpp>
 #include "AdMiningTask.h"
+
+using namespace izenelib::ir::be_index;
 
 namespace sf1r
 {
-using izenelib::ir::be_index::DNF;
 class DNFParser
 {
 public:
