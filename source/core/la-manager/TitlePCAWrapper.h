8f24cc7d2ce6248c7e75dd9b4800c017a1a67022
ops@B5M-D21 <ops@B5M-D21.com>
2013-12-31 16:37:54 +0800

    add product forward

---
+++ source/core/la-manager/TitlePCAWrapper.h
@@ -0,0 +1,49 @@
+/**
+ * @file TitlePCAWrapper.h
+ * @brief a wrapper class for ilplib::knlp functions.
+ */
+
+#ifndef SF1R_TITLE_PCA_WRAPPER_H
+#define SF1R_TITLE_PCA_WRAPPER_H
+
+#include <vector>
+#include <map>
+#include <string>
+#include <boost/scoped_ptr.hpp>
+
+namespace ilplib
+{
+namespace knlp
+{
+    class TitlePCA;
+}
+}
+
+namespace sf1r
+{
+
+class TitlePCAWrapper
+{
+public:
+
+    static TitlePCAWrapper* get();
+
+    TitlePCAWrapper();
+    ~TitlePCAWrapper();
+
+    bool loadDictFiles(const std::string& dictDir);
+    void pca(std::string line,
+         std::vector<std::pair<std::string, float> >& tks,
+         std::string& brand,
+         std::string& model_type,
+         std::vector<std::pair<std::string, float> >& sub_tks, bool do_sub)const;
+
+private:
+    std::string dictDir_;
+    bool isDictLoaded_;
+    boost::scoped_ptr<ilplib::knlp::TitlePCA> title_pca_;
+};
+
+} // namespace sf1r
+
+#endif // SF1R_TITLE_PCA_WRAPPER_H
