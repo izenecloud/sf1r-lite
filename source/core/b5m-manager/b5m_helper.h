5276f7fa4be99b82b13c5e39ed0d5f3b5b2b3475
Guoer <guoer@b5m.com>
2014-03-17 12:56:54 +0800

    bug fix, do not use static variable in Uint128ToString

--- source/core/b5m-manager/b5m_helper.h
+++ source/core/b5m-manager/b5m_helper.h
@@ -61,7 +61,7 @@ public:
 
     static std::string Uint128ToString(const uint128_t& val)
     {
-        static char tmpstr[33];
+        char tmpstr[33];
         sprintf(tmpstr, "%016llx%016llx", (unsigned long long) (val >> 64), (unsigned long long) val);
         return std::string(reinterpret_cast<const char *>(tmpstr), 32);
     }
