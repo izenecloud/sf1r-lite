738e73d858800508e5ff94f5cfa7023f5cf5f013
Yingfeng Zhang <yingfeng.zhang@gmail.com>
2014-09-22 20:32:16 +0800

    Fix config

--- test/TestResources.h.cmake
+++ test/TestResources.h.cmake
@@ -1,21 +1,8 @@
 #ifndef INCLUDED_TEST_RESOURCES_H
 #define INCLUDED_TEST_RESOURCES_H
 
-#ifndef WISEKMA_KNOWLEDGE
-# define WISEKMA_KNOWLEDGE "@wisekma_KNOWLEDGE@"
-#endif
-
 #ifndef IZENECMA_KNOWLEDGE
 # define IZENECMA_KNOWLEDGE "@izenecma_KNOWLEDGE@"
-#endif
-
-#ifndef MATCHER_KNOWLEDGE
-# define MATCHER_KNOWLEDGE "@EXTRA_CMAKE_MODULES_DIRS@/../b5m-operation/db/working_b5m/knowledge/"
-#endif
-
-
-#ifndef MOBILE_SOURCE
-# define MOBILE_SOURCE "@EXTRA_CMAKE_MODULES_DIRS@/../b5m-operation/config/collection/mobile_source.txt";
 #endif
 
 #endif // INCLUDED_TEST_RESOURCES_H
